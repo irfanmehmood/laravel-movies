d3941c6a066070c2052da714d3cc8009d01d13eb
irfan mehmood <irfmehmood@googlemail.com>
2020-10-09 00:41:35 +0100

    Installed Laravel Mix, Tailwind.  First dev build.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,9 @@
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')],
+    });
